cee6e788cde0f93f6b156bbeaa7340a3c5cc5cf1
Nayon-s <106899651+Nayon-s@users.noreply.github.com>
2024-06-20 21:22:08 +0600

    Update StudentsInfo.js

--- src/components/Admin/StudentsInfo.js
+++ src/components/Admin/StudentsInfo.js
@@ -22,7 +22,7 @@ const StudentsInfo = () => {
   <img src={index.image} class="card-img-top h-200" alt="..."/>
   <div className="card-body">
     <h5 className="card-title my-3">{index.name}</h5>
-    <h5 className="card-title my-3">No. of Students: {index.numberOfStudents}</h5>
+    <h5 className="card-title my-3">Total No. of Students: {index.numberOfStudents}</h5>
     <Link className="btn btn-dark " aria-current="page" to="/admin/studentlist">View More Info</Link>
 
   </div>
